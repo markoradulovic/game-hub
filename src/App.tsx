8828db4c0509e0d2417b36ef81f777070e63b70e
Marko Radulovic <markoradulovic012@gmail.com>
2023-05-15 20:16:03 +0200

    displaying the genres

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,13 @@ const App = () => {
   return (
     <Grid
       gridTemplateAreas={{ base: `"nav" "main"`, lg: `"nav nav" "aside main"` }}
+      gridTemplateColumns={{ base: '1fr', lg: '200px 1fr' }}
     >
       <GridItem area="nav">
         <NavBar />
       </GridItem>
       <Show above="lg">
-        <GridItem area="aside">
+        <GridItem area="aside" paddingX={5}>
           <GenreList />
         </GridItem>
       </Show>
