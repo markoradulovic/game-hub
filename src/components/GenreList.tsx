e19492494bc42c926b9da016d010fc8cea270b10
Marko Radulovic <markoradulovic012@gmail.com>
2023-05-15 20:28:05 +0200

    show genre skeletons

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,12 +1,28 @@
-import { HStack, Image, List, ListItem, Text } from '@chakra-ui/react';
+import {
+  HStack,
+  Image,
+  List,
+  ListItem,
+  SkeletonText,
+  Text,
+} from '@chakra-ui/react';
 import useGenres from '../hooks/useGenres';
 import getCroppedImageUrl from '../services/image-url';
 
 const GenreList = () => {
-  const { data } = useGenres();
+  const { data, error, isLoading } = useGenres();
+  const skeletons = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  if (error) return null;
 
   return (
     <List>
+      {isLoading &&
+        skeletons.map(skeleton => (
+          <ListItem key={skeleton} paddingY={5}>
+            <SkeletonText noOfLines={2} width={20} />
+          </ListItem>
+        ))}
       {data.map(genre => (
         <ListItem key={genre.id} paddingY="5px">
           <HStack>
